82f628c685f694f867fb5e81c904b841dadc5ccf
Williams Olivera <liamsolivera@gmail.com>
2015-07-28 10:15:24 -0400

    defined config file

---
+++ config/config.js
@@ -0,0 +1,15 @@
+
+
+/*********************** DATABASE CONFIGURATION LOCALHOST ****************************/
+
+var databaseUrl = 'localhost:27017/node-express-mongo-seed';
+
+var port = 2791;
+
+exports.data = {
+	port: port,
+	databaseUrl : databaseUrl,
+	secret: "jdZKYKwfTw9D9YMr",
+	redisHost : "localhost",//"107.155.116.xxx",
+	redisPort: 6379
+};
