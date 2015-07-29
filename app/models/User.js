f0f158a113207493ba2f196f9b7cb90eac32e538
Williams Olivera <liamsolivera@gmail.com>
2015-07-28 22:23:53 -0300

    defined User schema

---
+++ app/models/User.js
@@ -0,0 +1,28 @@
+var mongoose 	= require('mongoose');
+var Schema 		= mongoose.Schema;
+var bcrypt 		= require('bcrypt');
+
+var UserSchema = new Schema({	
+	email: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	firstName: {
+		type: String,
+		required: true
+	},
+	lastName: {
+		type: String,
+		required: true
+	},
+	timeCreated: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+module.exports = mongoose.model('User', UserSchema);
