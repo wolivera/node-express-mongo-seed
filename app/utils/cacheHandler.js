0e590592c583f0bfd2a7e77be69229b6ce6ddcac
Williams Olivera <liamsolivera@gmail.com>
2015-08-02 20:33:53 -0300

    implemented redis wrapper

---
+++ app/utils/cacheHandler.js
@@ -0,0 +1,23 @@
+var redis  = require('redis');
+
+exports.get = function(key, done){
+
+	getClient().get(key, done);	
+}
+
+exports.set = function(key, value){
+
+	getClient().set(key, value, redis.print);
+}
+
+exports.del = function(key, done){
+
+	var cli = getClient();
+
+	cli.get(key, function (err, u){
+		if(err){ return done(err); }
+		
+		cli.del(key);
+		done();
+	});
+}
