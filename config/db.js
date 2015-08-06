064d491fa1f71595c3cda66e84812d2b0d253313
Williams Olivera <liamsolivera@gmail.com>
2015-08-06 20:55:53 -0300

    improved mongoose connection termination

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-
+var config = require('./config');
 
 exports.connect = function() {
 
@@ -9,4 +9,26 @@ exports.connect = function() {
 	};
 	
 	handleConnection();
+
+	// Error handler
+	mongoose.connection.on('error', function(err) {
+		console.log(err);
+	});
+
+	// Reconnect when closed
+	mongoose.connection.on('disconnected', function() {
+		handleConnection();
+	});
+
+	// If the Node process ends, close the Mongoose connection
+	process.on('SIGINT', function() {
+		mongoose.connection.close(function () {
+			console.log('Mongoose default connection disconnected through app termination');
+			process.exit(0);
+		});
+	});
 };
+
+exports.sync = function(){
+	require("../app/models/User");
+}
