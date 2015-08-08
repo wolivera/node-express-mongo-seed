fe7c54bed6f933f0d53cc799a4386f5984f8024b
Williams Olivera <liamsolivera@gmail.com>
2015-08-08 20:49:53 -0300

    added api routes

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,4 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var userCtrl = require('../controllers/user');
+
+/*
+	Users routes (host:port/users/)
+*/
+
+router.post('/', userCtrl.signup);
+
+router.post('/login', userCtrl.login);
+
+router.post('/logout', userCtrl.logout);
+
 
 module.exports = router;
