3fabb0c332acec1b5ed091df0258485cc795e1ae
Williams Olivera <liamsolivera@gmail.com>
2015-08-20 21:52:53 -0300

    corrections on app js file

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-var config = require('./config/config.js');
+var config = require('./config/config');
+var server = require('./config/server');
+var router = require('./router');
+var db = require('./config/db');
 var express = require('express');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var flash = require('connect-flash');
 
 var app = express();
 /*
@@ -21,10 +29,11 @@ router.route(app);
 /*
 	Start server
 */
-app.set('port', (process.env.PORT || 2791));
+app.set('port', config.server.port);
 
 app.listen(app.get('port'), function() {
 	console.log('Server listenting on ' + process.pid + " and port " + app.get('port'));
 });
 
+// export app so we can test it
 exports = module.exports = app;
