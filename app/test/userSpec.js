adc3359e920849e735cef0f88d4527e532d5216e
Williams Olivera <liamsolivera@gmail.com>
2015-08-24 23:55:53 -0300

    test case added

--- app/test/userSpec.js
+++ app/test/userSpec.js
@@ -93,6 +93,28 @@ describe('User Controller', function() {
 		      	});
 		});
 
+		it('should return 400 and an error for missing parameters', function (done) {
+
+			request
+				.post(url + '/users')
+				.set({})
+				.send({
+			      	password 	: "test123",
+			      	firstName	: "test",
+			      	lastName 	: "user"
+				})
+				.end(function (err, res){
+			        expect(res).to.exist;
+			        expect(res.status).to.equal(400);
+			        expect(res.body.errors).to.exist;
+			        expect(res.body.errors[0]).to.exist;
+			        expect(res.body.errors[0].message).to.equal("Missing parameters");
+
+			        done();
+		      	});
+
+		});
+
 	});
 
 });
